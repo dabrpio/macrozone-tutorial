8d29ca7414d9ebee5d91ac986cede46eddd5b855
dabrpio <pdvbrowski@gmail.com>
2026-08-20 21:37:45 +0200

    add routing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import HomeHeader from '@/components/home-header';
 import { globalStyles } from '@/styles/global';
+import { Link } from 'expo-router';
 import { ScrollView, Text } from 'react-native';
 
 export default function HomeScreen() {
@@ -7,6 +8,9 @@ export default function HomeScreen() {
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
+      <Link href="/meals" style={{ fontSize: 18, color: '#007bff' }}>
+        Go to Meals
+      </Link>
     </ScrollView>
   );
 }
