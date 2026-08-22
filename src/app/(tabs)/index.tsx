ce14d0def1625279e03e9873a4f9b6bedbe2e722
dabrpio <pdvbrowski@gmail.com>
2026-08-22 20:03:36 +0200

    add recent meals component with meal items

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import HomeHeader from '@/components/home-header';
 import MacroGrid from '@/components/macro-grid';
+import RecentMeals from '@/components/recent-meals';
 import { globalStyles } from '@/styles/global';
 import { ScrollView, Text } from 'react-native';
 
@@ -9,6 +10,7 @@ export default function HomeScreen() {
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
       <MacroGrid />
+      <RecentMeals />
     </ScrollView>
   );
 }
