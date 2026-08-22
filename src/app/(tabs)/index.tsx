83f6e742d89288fdc993834efde8d49e791b5bd5
dabrpio <pdvbrowski@gmail.com>
2026-08-22 20:35:13 +0200

    display data from storage in recent meals

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,33 @@
 import HomeHeader from '@/components/home-header';
 import MacroGrid from '@/components/macro-grid';
 import RecentMeals from '@/components/recent-meals';
+import { Meal, getMeals } from '@/storage/meals';
 import { globalStyles } from '@/styles/global';
+import { useFocusEffect } from 'expo-router';
+import { useCallback, useState } from 'react';
 import { ScrollView, Text } from 'react-native';
 
 export default function HomeScreen() {
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+    console.log('Loaded meals:', data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, []),
+  );
+
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
       <MacroGrid />
-      <RecentMeals />
+      <RecentMeals meals={meals} />
     </ScrollView>
   );
 }
