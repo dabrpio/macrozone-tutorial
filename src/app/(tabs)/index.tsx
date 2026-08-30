ce6b3e8bb04b7d55c812dce6fc054c6f28fc3375
dabrpio <pdvbrowski@gmail.com>
2026-08-30 18:44:25 +0200

    add share button

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
 import HomeHeader from '@/components/home-header';
 import MacroGrid from '@/components/macro-grid';
 import RecentMeals from '@/components/recent-meals';
+import ShareButton from '@/components/share-button';
 import { Meal, getMeals } from '@/storage/meals';
 import { globalStyles } from '@/styles/global';
 import { useFocusEffect } from 'expo-router';
 import { useCallback, useState } from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 
 export default function HomeScreen() {
   const [meals, setMeals] = useState<Meal[]>([]);
@@ -24,7 +25,10 @@ export default function HomeScreen() {
 
   return (
     <ScrollView style={globalStyles.container}>
-      <Text style={globalStyles.title}>MacroZone</Text>
+      <View style={globalStyles.header}>
+        <Text style={globalStyles.title}>MacroZone</Text>
+        <ShareButton meals={meals} />
+      </View>
       <HomeHeader />
       <MacroGrid meals={meals} />
       <RecentMeals meals={meals} onDelete={loadMeals} />
